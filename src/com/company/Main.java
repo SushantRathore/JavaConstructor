65fa2ebf4cc5953c43ec84dc42cc4314dba56d14
Sushant-Singh-Rathore <ssr7705@gmail.com>
2020-05-15 01:50:14 +0530

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        testing obj = new testing(7);
+        obj = new testing( );
+    }
+}
---
+++ src/com/company/testing.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class testing {
+
+    int i ;
+    int j;
+
+    testing ()
+    {
+        System.out.println("default constructor");
+    }
+
+    testing(int k)
+    {
+        System.out.println("patamerized constructor");
+    }
+}
